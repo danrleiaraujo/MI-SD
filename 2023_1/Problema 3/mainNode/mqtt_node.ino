a3d29726aa02643e58ebf44efdd95ab53037eb26
Evelynsuzarte <evelynsuzarte@hotmail.com>
2023-06-02 16:56:21 -0300

    mqtt

--- 2023_1/Problema 3/mainNode/mqtt_node.ino
+++ 2023_1/Problema 3/mainNode/mqtt_node.ino
@@ -1,64 +1,178 @@
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
 
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
+# include <ESP8266WiFi.h>
+# include <ESP8266mDNS.h>
+# include <WiFiUdp.h>
+# include <ArduinoOTA.h>
+# include <PubSubClient.h>
 
+# ifndef STASSID
+# define STASSID "INTELBRAS"
+# define STAPSK  "Pbl-Sistemas-Digitais"
+# endif
 
-// MQTT Broker
-const char *mqtt_broker = "10.0.0.1";
-const char *topic = "esp8266/test";
-const char *mqtt_username = "aluno";
-const char *mqtt_password = "@luno*123";
-const int mqtt_port = 1883;
+const char* ssid = STASSID; 
+const char* password = STAPSK;
+const char* host = "ESP-10.0.0.107";
 
+int led_pin = LED_BUILTIN;
 
-//Defina o agente MQTT, escreva a função de retorno de chamada e
-// imprima as informações de conexão no monitor serial ao mesmo tempo.
+int buttonD0 = D0; // push button is connected
+int buttonD1 = D1; // push button is connected
+int temp0 = 0;   // temporary variable for reading the button pin status
+int temp1 = 0;   // temporary variable for reading the button pin status
 
-client.setServer(mqtt_broker, mqtt_port);
-client.setCallback(callback);
-while (!client.connected()) {
-    String client_id = "esp8266-client-";
-    client_id += String(WiFi.macAddress());
-    Serial.printf("O cliente %s se conecta ao intermediário mqtt público\n", client_id.c_str());
-    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+// MQTT broker credentials
+const char* MQTT_username = "aluno"; 
+const char* MQTT_password = "@luno*123"; 
+
+// MQTT broker
+const char* mqtt_server = "10.0.0.101";
+
+// Client MQTT
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+# define N_DIMMERS 3
+int dimmer_pin[] = {14, 5, 15};
+
+/* POTENCIOMETRO */
+int analogInPin = A0;
+int sensorValue = 0;  // value read from the pot
+/* POTENCIOMETRO */
+char arrb[]={0b00000000, 0b00000001, 0b00000010, 0b00000011, 0b00000100};      
+
+char protocolCodes[] = {
+0b00000000, 0b00000001, 0b00000010, 0b00000011, 0b00000100, 0b00000101, 0b00000110, 0b00000111, 0b00001000, 0b00001001,
+0b00001010, 0b00001011, 0b00001100, 0b00001101, 0b00001110, 0b00001111, 0b00010000, 0b00010001, 0b00010010, 0b00010011,
+0b00010100, 0b00010101, 0b00010110, 0b00010111, 0b00011000, 0b00011001, 0b00011010, 0b00011011, 0b00011100, 0b00011101,
+0b00011110, 0b00011111, 0b00100000, 0b00100001, 0b00100010, 0b00100011, 0b00100100, 0b00100101, 0b00100110, 0b00100111,
+0b00101000, 0b00101001, 0b00101010, 0b00101011, 0b00101100, 0b00101101, 0b00101110, 0b00101111, 0b00110000, 0b00110001,
+0b00110010, 0b00110011, 0b00110100, 0b00110101, 0b00110110, 0b00110111, 0b00111000, 0b00111001, 0b00111010, 0b00111011,
+0b00111100, 0b00111101, 0b00111110, 0b00111111
+};
+
+char toRequest[] = { 0b00100010, 0b00100011, 0b00100100, 0b00100101, 0b00100110, 0b00100111, 0b00101000, 0b00101001, 0b00101010 };
+
+void setup() {
+
+  // pinMode(D0, INPUT);
+  /* BaudRate */
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
+  }
+  /* switch off led */
+  digitalWrite(led_pin, LOW);
+
+  /* configure dimmers, and OTA server events */
+  analogWriteRange(1000);
+  analogWrite(led_pin, 990);
+
+  for (int i = 0; i < N_DIMMERS; i++) {
+    pinMode(dimmer_pin[i], OUTPUT);
+    analogWrite(dimmer_pin[i], 50);
+  }
+
+  ArduinoOTA.setHostname(host);
+  ArduinoOTA.onStart([]() { // switch off all the PWMs during upgrade
+    for (int i = 0; i < N_DIMMERS; i++) {
+      analogWrite(dimmer_pin[i], 0);
+    }
+    analogWrite(led_pin, 0);
+  });
+
+  ArduinoOTA.onEnd([]() { // do a fancy thing with our board led at end
+    for (int i = 0; i < 30; i++) {
+      analogWrite(led_pin, (i * 100) % 1001);
+      delay(50);
+    }
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    (void)error;
+    ESP.restart();
+  });
+
+  /* setup the OTA server */
+  ArduinoOTA.begin();
+
+  pinMode(led_pin, OUTPUT); /* configura LED embutido como saída */  
+  pinMode(buttonD0, INPUT); /* configura D0 embutido como entrada (botão) */    
+  pinMode(buttonD1, INPUT); /* configura D1 embutido como entrada (botão) */    
+  
+  client.setServer(mqtt_server, 1883); 
+  client.setCallback(callback);
+}
+
+// This function is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
+// When a device publishes a message to a topic that 
+// your ESP8266 is subscribed you can actually do something
+void callback(String topic, byte* message, unsigned int length) {
+  if(topic == "comandos"){
+    String messageTemp, value;
+  
+    for (int i = 0; i < length; i++) {
+      messageTemp += (char)message[i];
+    }
+
+    if(messageTemp == "0b00100001"){ // Alternar estado do led
+      if(digitalRead(led_pin) == HIGH){
+        digitalWrite(led_pin, LOW);
+      }
+      else if(digitalRead(led_pin) == LOW){
+        digitalWrite(led_pin, HIGH);
+      }
+      value = "0b00000010"; //ok
+      // Publishes the read value
+      delay(1500);
+      client.publish("respostas", String(value).c_str());
+    }
+
+    /*
+    Testar 
+    readSensors(messageTemp);
+    */
+  }
+}
+
+// This functions reconnects your ESP8266 to your MQTT broker
+// Change the function below if you want to subscribe to more topics with your ESP8266 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    // Attempt to connect
+    if (client.connect("ESP8266Client", MQTT_username, MQTT_password)) {
+      // Subscribe or resubscribe to a topic
+      // You can subscribe to more topics (to control more LEDs in this example)
+      client.subscribe("comandos");
     } else {
-        Serial.print("falha com o estado ");
-        Serial.print(client.state());
-        delay(2000);
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
+  }
 }
-
-void callback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Mensagem chegou no tópico:  ");
-    Serial.println(topic);
-    Serial.print("Mensagem:");
-    for (int i = 0; i < length; i++) {
-        Serial.print((char) payload[i]);
-    }
-    Serial.println();
-    Serial.println("-----------------------");
+/*
+void readSensors(){             
+  if(pc == toRequest[3] ){ 
+    digitalWrite(led_pin, LOW);
+  }
+  if(pc == toRequest[4] ){  
+    digitalWrite(led_pin, HIGH);
+  }      
 }
+*/
+void loop() {
+  ArduinoOTA.handle();
 
+  if (!client.connected()) {
+  reconnect();
+  }
 
-//Depois de se conectar com sucesso ao broker MQTT, o ESP8266 publicará mensagens e assinará o broker MQTT.
-
-
-// publish and subscribe
-client.publish(topic, "hello emqx");
-client.subscribe(topic);
-
-
-//Imprima o nome do tópico na porta serial e, em seguida, imprima cada byte de mensagens recebidas.
-void callback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Mensagem chegou no tópico: ");
-    Serial.println(topic);
-    Serial.print("Mensagem:");
-    for (int i = 0; i < length; i++) {
-        Serial.print((char) payload[i]);
-    }
-    Serial.println();
-    Serial.println("-----------------------");
+  if(!client.loop())
+    client.connect("ESP8266Client", MQTT_username, MQTT_password);  
 }
--- 2023_1/Problema 3/mqtt.c
+++ 2023_1/Problema 3/mqtt.c
@@ -1,94 +1,161 @@
-* Includes
-*/
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <MQTTClient.h>
+#include "MQTTClient.h"
+
+//dontpad dopbl
  
-/*
-* Defines
-*/
-#define MQTT_ADDRESS   "tcp://10.0.0.101:1883@@luno*123"
-#define CLIENTID       "aluno"  
+#define ADDRESS     "tcp://10.0.0.101:1883@@luno*123"
+#define CLIENTID_PUB    "SBC"
+#define CLIENTID_SUB    "NODEMCU"
 #define USERNAME	"aluno"
 #define PASSWORD	"@luno*123"
-#define MQTT_PUBLISH_TOPIC     "MQTTCClientPubTopic"
-#define MQTT_SUBSCRIBE_TOPIC   "MQTTCClientSubTopic"
+
+#define MQTT_PUBLISH_TOPIC     "comandos"
+#define MQTT_SUBSCRIBE_TOPIC   "respostas"
+
+/* binário */
+#define PAYLOAD     "0b00100001" // Alternar estado do led 
+
+#define QOS         1
+#define TIMEOUT     10000L
+
+volatile MQTTClient_deliveryToken deliveredtoken;
+
+void delivered(void *context, MQTTClient_deliveryToken dt);
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message);
+void connlost(void *context, char *cause);
+int publisher();
+int subscriber();
  
-/*
-*  Variaveis globais
-*/
-MQTTClient client;
- 
-/*
-* Prototipos de funcao
-*/
-void publish(MQTTClient client, char* topic, char* payload);
-int on_message(void *context, char *topicName, int topicLen, MQTTClient_message *message);
- 
-/*
-* Implementacoes
-*/
- 
-/* Funcao: publicacao de mensagens MQTT
-*/
-void publish(MQTTClient client, char* topic, char* payload) {
-    MQTTClient_message pubmsg = MQTTClient_message_initializer;
- 
-    pubmsg.payload = payload;
-    pubmsg.payloadlen = strlen(pubmsg.payload);
-    pubmsg.qos = 2;
-    pubmsg.retained = 0;
-    MQTTClient_deliveryToken token;
-    MQTTClient_publishMessage(client, topic, &pubmsg, &token);
-    MQTTClient_waitForCompletion(client, token, 1000L);
+int main(int argc, char* argv[])
+{
+    publisher();
+    delay(1000);
+    subscriber();
+}
+
+void delivered(void *context, MQTTClient_deliveryToken dt)
+{
+    printf("Message with token value %d delivery confirmed\n", dt);
+    deliveredtoken = dt;
 }
  
-/* Funcao: callback de mensagens MQTT recebidas e echo para o broker
- * Parametros: contexto, ponteiro para nome do topico da mensagem recebida, 
- * tamanho do nome do topico e mensagem recebida
- * Retorno : 1: sucesso (fixo / nao ha checagem de erro neste exemplo)
-*/
-int on_message(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
-    char* payload = message->payload;
- 
-    /* Mostra a mensagem recebida */
-    printf("Mensagem recebida! \n\rTopico: %s Mensagem: %s\n", topicName, payload);
- 
-    /* Faz echo da mensagem recebida */
-    publish(client, MQTT_PUBLISH_TOPIC, payload);
- 
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
+{
+    printf("Message arrived\n");
+    printf("     topic: %s\n", topicName);
+    printf("   message: %.*s\n", message->payloadlen, (char*)message->payload);
     MQTTClient_freeMessage(&message);
     MQTTClient_free(topicName);
     return 1;
 }
  
- 
-int main(int argc, char *argv[])
+void connlost(void *context, char *cause)
 {
-   int rc;
-   MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    printf("\nConnection lost\n");
+    printf("     cause: %s\n", cause);
+}
  
-   /* Inicializacao do MQTT (conexao & subscribe) */
-   MQTTClient_create(&client, MQTT_ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-   MQTTClient_setCallbacks(client, NULL, NULL, on_message, NULL);
+int publisher(){
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;
+        conn_opts.keepAliveInterval = 20;
+        conn_opts.cleansession = 1;
+        conn_opts.username = USERNAME;
+        conn_opts.password = PASSWORD;
+    int rc;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID_PUB,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL);
+
+
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(EXIT_FAILURE);
+    }
  
-   rc = MQTTClient_connect(client, &conn_opts);
+    pubmsg.payload = PAYLOAD;
+    pubmsg.payloadlen = (int)strlen(PAYLOAD);
+    pubmsg.qos = QOS;
+    pubmsg.retained = 0;
+    MQTTClient_publishMessage(client, MQTT_PUBLISH_TOPIC, &pubmsg, &token);
  
-   if (rc != MQTTCLIENT_SUCCESS)
-   {
-       printf("\n\rFalha na conexao ao broker MQTT. Erro: %d\n", rc);
-       exit(-1);
-   }
+    printf("Waiting for up to %d seconds for publication of %s\n"
+            "on topic %s for client with ClientID: %s\n",
+            (int)(TIMEOUT/1000), PAYLOAD, MQTT_PUBLISH_TOPIC, CLIENTID_PUB);
+    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+    printf("Message with delivery token %d delivered\n", token);
+    rc = MQTTClient_disconnect(client, 10000);
+
+    MQTTClient_destroy(&client);
+    return rc;
+}
+
+int subscriber(){
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+        conn_opts.keepAliveInterval = 20;
+        conn_opts.cleansession = 1;
+        conn_opts.username = USERNAME;
+        conn_opts.password = PASSWORD;
+    int rc;
  
-   MQTTClient_subscribe(client, MQTT_SUBSCRIBE_TOPIC, 0);
+    if ((rc = MQTTClient_create(&client, ADDRESS, CLIENTID_SUB,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to create client, return code %d\n", rc);
+        rc = EXIT_FAILURE;
+        goto exit;
+    }
  
-   while(1)
-   {
-       /*
-        * o exemplo opera por "interrupcao" no callback de recepcao de 
-        * mensagens MQTT. Portanto, neste laco principal eh preciso fazer
-        * nada.
-        */
-   }
+    if ((rc = MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to set callbacks, return code %d\n", rc);
+        rc = EXIT_FAILURE;
+        goto destroy_exit;
+    }
+ 
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to connect, return code %d\n", rc);
+        rc = EXIT_FAILURE;
+        goto destroy_exit;
+    }
+ 
+    printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
+           "Press Q<Enter> to quit\n\n", MQTT_SUBSCRIBE_TOPIC, CLIENTID_SUB, QOS);
+    if ((rc = MQTTClient_subscribe(client, MQTT_SUBSCRIBE_TOPIC, QOS)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to subscribe, return code %d\n", rc);
+        rc = EXIT_FAILURE;
+    }
+    else
+    {
+        int ch;
+        do
+        {
+                ch = getchar();
+        } while (ch!='Q' && ch != 'q');
+ 
+        if ((rc = MQTTClient_unsubscribe(client, MQTT_SUBSCRIBE_TOPIC)) != MQTTCLIENT_SUCCESS)
+        {
+                printf("Failed to unsubscribe, return code %d\n", rc);
+                rc = EXIT_FAILURE;
+        }
+    }
+ 
+    if ((rc = MQTTClient_disconnect(client, 10000)) != MQTTCLIENT_SUCCESS)
+    {
+        rc = EXIT_FAILURE;
+    }
+    destroy_exit:
+        MQTTClient_destroy(&client);
+    exit:
+    return rc;
 }
