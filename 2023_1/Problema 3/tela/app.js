9801fb1dfc0045dde67278a48a08bc7c5c782203
Evelynsuzarte <evelynsuzarte@hotmail.com>
2023-06-16 02:06:08 -0300

    front

--- 2023_1/Problema 3/tela/app.js
+++ 2023_1/Problema 3/tela/app.js
@@ -1,17 +1,8 @@
-const labels = [
-    'node1',
-    'node2'
-];
+// const labels = [
+//     'node1',
+//     'node2'
+// ];
 
-const data = {
-    labels: labels,
-    datasets: [{
-        data: [0 , 1 , 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100],
-        label: "Valor do sensor",
-        backgroundColor: "purple",
-        borderColor: "purple"
-    }]
-};
 
 function add_data(label, data) {
     myChart.data.labels.push(label);
@@ -31,7 +22,11 @@ function remove_firstData() {
 const ctx = document.getElementById('myChart').getContext("2d");
 const myChart = new Chart(ctx, {
     type: 'line',
-    data: data,
+    data: {
+        datasets: [{ label: "Valor do sensor", }],
+        backgroundColor: "purple",
+        borderColor: "purple",
+    },
     options:{
         resposive: true
     }
@@ -39,6 +34,66 @@ const myChart = new Chart(ctx, {
 
 
 const contador_max = 10;
-console.log("Dado recebido: data/ Node x/ pino/ informação:", msg.data + " - " + msg.node + "----->"+ msg.pino + " :: " + msg.informacao);
-XMLDocument
 
+const mqtt = require('mqtt')
+const url = 'tcp://10.0.0.101:1883@@luno*123'
+const topic = 'front'
+const resultado = []
+
+// Cria configuração de conexão mqtt
+const options = {
+  clean: true,
+  clientId: 'SBC',
+  username: 'aluno',
+  password: '@luno*123',
+}
+const client  = mqtt.connect(url, options)
+
+client.on('connect', function () {
+    if(!err){
+        console.log('------------ Conectado! -------------');
+        client.subscribe(topic);
+    } else{
+        console.log("Não conectado!");
+    }
+});
+
+client.on(topic, function (message) {
+    console.log(message.toString());
+    let messageTemp = message.toString();
+
+    resultado = messageTemp.split(",", 4);  //data/ Node x/ pino/ informação
+    // var data = resultado[0]
+    // var node = resultado[1]
+    // var pino = resultado[2]
+    // var informacao = resultado[3]
+
+    console.log("Recebendo:", + resultado[0] + "Node: " + 
+                resultado[1] + "----->" + resultado[2] + " : " + resultado[3]);
+
+    if(myChart.data.labels.length > contador_max) {
+        remove_firstData();
+    }
+    add_data(msg.date, msg.value);
+
+    client.end();
+});
+
+
+
+
+
+
+
+
+
+
+
+// Receive messages
+// client.on('message', function (topic, message) {
+//   // message is Buffer
+//   console.log(message.toString())
+//   client.end()
+// })
+
+//https://www.emqx.com/en/blog/mqtt-js-tutorial
---
+++ 2023_1/Problema 3/tela/teste.js
@@ -0,0 +1,9 @@
+// Array retornado: ["João", "da", "Silva"]
+stringExemplo = "João da Silva Oliveira";
+resultado = stringExemplo.split(" ", 5);
+//console.log(resultado)
+
+var message = "1556jadj"
+console.log(message.toString())
+let messageTemp = message.toString()
+console.log("temporario",messageTemp)
